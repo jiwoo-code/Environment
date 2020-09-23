f5572509b189cb716f2bcaeb43a871d9c82f129d
Park Jiwoo <chryso@naver.com>
2020-09-23 13:39:22 +0900

    한솔씨가 알려준 방식으로 싱글턴 처리

---
+++ EnvironmentTest/Environment/ExecutionEnvironment3.cs
@@ -0,0 +1,75 @@
+﻿using EnvironmentTest.Common;
+using EnvironmentTest.Common.Utilities;
+using EnvironmentTest.Environment.Interface;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EnvironmentTest.Environment
+{
+    public class ExecutionEnvironment3<T> where T : IEnvironment, new()
+    {
+        private static readonly Lazy<ExecutionEnvironment3<T>> instance = new Lazy<ExecutionEnvironment3<T>>(() => new ExecutionEnvironment3<T>());
+
+        public static T InstanceEnv
+        {
+            get
+            {
+                return instance.Value.Env;
+            }
+        }
+
+        private readonly string EnvironmentFilePath;
+
+        public T Env { get; set; }
+
+        private ExecutionEnvironment3()
+        {
+            Env = new T();
+            EnvironmentFilePath = Path.Combine(Define.ConfDirPath, $"{typeof(T).Name}.json");
+
+            // 환경설정 폴더 없으면 생성
+            DirectoryUtilities.CreateDirectory(Define.ConfDirPath);
+
+            // 설정파일 확인
+            if (File.Exists(EnvironmentFilePath))
+            {
+                Read();
+            }
+            else
+            {
+                Env.Initialize();
+                Save();
+            }
+        }
+
+        /// <summary>
+        /// 환경설정 파일 불러와 값 설정하기
+        /// </summary>
+        public void Read()
+        {
+            using (StreamReader r = new StreamReader(EnvironmentFilePath))
+            {
+                string json = r.ReadToEnd();
+                Env = JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+            }
+        }
+
+        /// <summary>
+        /// 설정 값을 파일로 저장하기
+        /// </summary>
+        public void Save()
+        {
+            if (string.IsNullOrWhiteSpace(EnvironmentFilePath))
+            {
+                string param = "string path";
+                throw new ArgumentNullException(param);
+            }
+
+            string jsonString = JsonConvert.SerializeObject(Env, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+            File.WriteAllText(EnvironmentFilePath, jsonString);
+        }
+    }
+}
--- EnvironmentTest/Program.cs
+++ EnvironmentTest/Program.cs
@@ -15,6 +15,30 @@ namespace EnvironmentTest
             Console.WriteLine(executionEnv1.Test1);
             Console.WriteLine(executionEnv2.TestB);
             Console.WriteLine(executionEnv3.Users);
+
+            var env1 = ExecutionEnvironment2<TestEnvironment1>.Instacne.Env;
+            var env2 = ExecutionEnvironment2<TestEnvironment2>.Instacne.Env;
+            var env3 = ExecutionEnvironment2<TestEnvironment3>.Instacne.Env;
+            var env4 = ExecutionEnvironment2<TestEnvironment4>.Instacne.Env;
+            var env5 = ExecutionEnvironment2<TestEnvironment4>.Instacne.Env;
+
+            env4.DBConnectionString = "1";
+            env5.DBConnectionString = "2";
+
+            var e1 = ExecutionEnvironment3<TestEnvironment1>.InstanceEnv;
+            var e2 = ExecutionEnvironment3<TestEnvironment2>.InstanceEnv;
+            var e3 = ExecutionEnvironment3<TestEnvironment3>.InstanceEnv;
+            var e4 = ExecutionEnvironment3<TestEnvironment4>.InstanceEnv;
+            var e5 = ExecutionEnvironment3<TestEnvironment4>.InstanceEnv;
+
+            e4.DBConnectionString = "1";
+            e5.DBConnectionString = "2";
+
+            Console.WriteLine( e1.GetHashCode().ToString());
+            Console.WriteLine( e2.GetHashCode().ToString());
+            Console.WriteLine( e3.GetHashCode().ToString());
+            Console.WriteLine( e4.GetHashCode().ToString());
+            Console.WriteLine( e5.GetHashCode().ToString());
         }
     }
 }
